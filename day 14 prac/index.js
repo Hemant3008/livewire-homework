8ee0a2e2b7f87f61c2ff99a38d7924fd0d158069
Hemant3008 <ghormadeh@gmail.com>
2023-06-08 15:49:19 +0530

    loop hw

---
+++ day 14 prac/index.js
@@ -0,0 +1,66 @@
+// // apply else if else 
+// // design a bank security system where user needs to enter username and password match them with 
+// // system username and password  print massage successfull ;
+
+// //fixed in system
+// var username =123456;
+// var password =987654;
+
+
+// //user input 
+// var entusername =123456;
+// var entpassword=987654;
+
+// if(username==entusername){
+//     console.log("username is correct enter password");
+//     if(password==entpassword){
+//         console.log("correct password login successfull")
+//     }else{
+//         console.log("please enter correct password")
+//     }
+// }else {
+//     console.log("please enter correct username")
+// }
+
+// design a system for atm where atm no and pin needs tobe match and print msg of teansaction successfull ; 
+
+// // fixed in system
+// var atmnumber = 232323;
+// var atmpin= 456456;
+
+// //user input
+// var entatmnumber= 232323;
+// var entatmpin= 456456;
+
+// if(atmnumber== entatmnumber){
+//     console.log("atmnumber is correct enter atmpin")
+//     if(atmpin == entatmpin){
+//         console.log (" atmpin is correct your transaction is successfull ")
+
+//     }else{
+//         console.log (" please enter correct atm pin")
+//     }
+// }else{
+//     console.log(" please enter correct atmnumber ")
+// }
+
+// // while loop 
+// // print all even numbers upto 50 ;
+// let i=1
+// while(i<=50){
+//     if ((i%2==0)) {
+//         console.log(i)
+//     } 
+//     i++;
+// }
+
+
+// while loop
+// print all odd numbers upto 50 ;
+let i=1
+while(i<=50){
+    if(i%2==1){
+        console.log(i)
+    }
+    i++
+}
