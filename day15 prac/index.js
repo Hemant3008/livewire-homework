0023e1fc467aa88354236b0dd3a8437e57745580
Hemant3008 <ghormadeh@gmail.com>
2023-06-08 15:50:52 +0530

    day15 hw

---
+++ day15 prac/index.js
@@ -0,0 +1,73 @@
+// desing pattern of  #
+// # # # # # # #
+// # # # # # #
+// # # # # #
+// # # # #
+// # # #
+// # #
+// #
+
+for (let i = 7; i >= 1; i--) {
+
+    let card = "";
+
+    for (let j = i; j >= 1; j--) {
+        card = card + "# ";
+    }
+
+    console.log(card);
+
+
+}
+
+// write a code for 10 factorial  using loop both while and for  ;
+
+// for loop
+
+for(let i= 10; i>=1; i--){
+    let cd= " ";
+    for(let j=i; j>=1; j--){
+        cd= cd* (i-1) ;}
+
+    console.log(cd)
+}
+
+
+// // write a switch case for car washing center where we have differnt types of cars SUV'S ,sedain , tuv , micro suv , hatchback , trucks ;
+// vehicle="truck";
+// switch(vehicle){
+//     case "suv" : console.log("this is suv")
+//                  break;
+//     case "sedian" : console.log("this is sedian") 
+//                     break;           
+//     case "tuv" : console.log("this is tuv")
+//                  break;
+//     case "micro suv" : console.log("this is micro suv") 
+//                     break;
+//     case "hatch back": console.log("this is hatch back")
+//                        break;
+//     case "truck" : console.log("this is truck")
+//                    break;
+//     default : console.log("Enter correct vehicles");
+// }
+
+//console 
+
+// design a report card of school livewire which contains
+// 5 subjects and there marks and percentage using concatination .
+
+
+// |-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+// | report card of live wire 
+// | -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+// |  name    class
+// | school   roll
+// |-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+{console.log ("|-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=")}
+{console.log("| reoprt card of live wire")}
+{console.log("|-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=")}
+{console.log("Name- Hemant Ghormade")}
+{console.log("class- 10")}
+{console.log("school- Late Devkibai Bang")}
+{console.log("Roll no- J107375")}
