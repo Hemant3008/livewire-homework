ddd253f3ac35786edca2416517a0537b5cffe32c
Hemant3008 <ghormadeh@gmail.com>
2023-06-16 16:38:50 +0530

    work done

---
+++ day18 prac/index.js
@@ -0,0 +1,45 @@
+// traverse the arr [5,10,6,9,85,7,9,2,7]
+// and  mul all the elements with 16
+
+// let arr= [5,10,6,9,85,7,9,2,7]
+// let alength= arr.length ;
+
+// for(i=0;i<arr.length;i++){
+//     arr[i]=arr[i]*16
+// }
+// console.log(arr);
+
+// output  [80, 160, 96, 144, 1360, 112, 144, 32, 112]
+
+
+// traverse the arr [10,20,30,40,50]
+// and print the addition of all elements of arr
+// let bag=0;
+// let arr= [10,20,30,40,50]
+// let alength= arr.length;
+
+// for(i=0;i<arr.length;i++){
+//         bag=bag+arr[i]
+//     }
+   
+// console.log(bag)
+
+
+// traverse the arr [10,20,30,40,50]
+// and  div all the element by 10
+
+// let arr=[10,20,30,40,50]
+// let alength= arr.length ;
+
+// for(i=0;i<arr.length;i++){
+//     arr[i]=arr[i]/10
+// }
+// console.log(arr);
+
+// traverse the arr [5,10,6,9,85,7,9,2,7]
+// find all even element
+// and also odd elements 
+let even=" "
+let arr= [5,10,6,9,85,7,9,2,7];
+if (arr[i] %!=0)
+
